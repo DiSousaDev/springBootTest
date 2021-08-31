e822833ede250887866f3d13cfe1845c0c68ac31
Eder Diego de Sousa <eder.diego@foursys.com.br>
2021-08-30 22:37:27 -0300

    Criando e implementarndo a Classe Service

---
+++ src/main/java/br/com/diego/springboottest/services/ContaService.java
@@ -0,0 +1,17 @@
+package br.com.diego.springboottest.services;
+
+import br.com.diego.springboottest.models.Conta;
+
+import java.math.BigDecimal;
+
+public interface ContaService {
+
+    Conta buscarPorId(Long id);
+
+    int consultarTotalTransferencia(Long bancoId);
+
+    BigDecimal consultarSaldo(Long contaId);
+
+    void efetuarTransferencia(Long contaOrigem, Long contaDestino, BigDecimal valor);
+
+}
---
+++ src/main/java/br/com/diego/springboottest/services/impl/ContaServiceImpl.java
@@ -0,0 +1,56 @@
+package br.com.diego.springboottest.services.impl;
+
+import br.com.diego.springboottest.models.Banco;
+import br.com.diego.springboottest.models.Conta;
+import br.com.diego.springboottest.repositories.BancoRepository;
+import br.com.diego.springboottest.repositories.ContaRepository;
+import br.com.diego.springboottest.services.ContaService;
+
+import java.math.BigDecimal;
+
+public class ContaServiceImpl implements ContaService {
+
+    private ContaRepository contaRepository;
+
+    private BancoRepository bancoRepository;
+
+    public ContaServiceImpl(ContaRepository contaRepository, BancoRepository bancoRepository) {
+        this.contaRepository = contaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public Conta buscarPorId(Long id) {
+        return contaRepository.findById(id);
+    }
+
+    @Override
+    public int consultarTotalTransferencia(Long bancoId) {
+        Banco banco = bancoRepository.findById(bancoId);
+        return banco.getTotalTransferencias();
+    }
+
+    @Override
+    public BigDecimal consultarSaldo(Long contaId) {
+        Conta conta = contaRepository.findById(contaId);
+        return conta.getSaldo();
+    }
+
+    @Override
+    public void efetuarTransferencia(Long numContaOrigem, Long numContaDestino, BigDecimal valor) {
+       Banco banco = bancoRepository.findById(1L);
+       int totalTransferencias = banco.getTotalTransferencias();
+       banco.setTotalTransferencias(++totalTransferencias);
+       bancoRepository.update(banco);
+
+       Conta contaOrigem = contaRepository.findById(numContaOrigem);
+       contaOrigem.debito(valor);
+       contaRepository.update(contaOrigem);
+
+       Conta contaDestino = contaRepository.findById(numContaDestino);
+       contaDestino.credito(valor);
+       contaRepository.update(contaDestino);
+
+    }
+
+}
